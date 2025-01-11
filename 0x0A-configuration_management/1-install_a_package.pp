d97beea0f08d5fa1bfa439524e113ba62558448c
Evans <ivanz.corbie@gmail.com>
2025-01-11 13:59:59 +0000

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,8 @@
-# This Puppet manifest installs a specific version of flask (2.1.0) using pip3
+# This Puppet manifest installs Flask version 2.1.0 and ensures compatibility with Werkzeug
 
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.1.2',
+  path    => '/usr/bin:/bin',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }
+
