ad9e691abac5002f4080daea9e8c3f27a5106eb8
Evans <ivanz.corbie@gmail.com>
2025-02-27 22:12:43 +0000

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,22 +1,6 @@
-# This Puppet manifest fixes missing PHP modules and restarts Apache
+# A puppet manifest that fixes the `phpp` extensions in the WordPress file `wp-settings.php` to `php`.
 
-class { 'apache':
-  service_ensure => 'running',
-}
-
-package { ['php5', 'libapache2-mod-php5', 'php5-mysql']:
-  ensure => installed,
-}
-
-exec { 'restart-apache':
-  command     => '/etc/init.d/apache2 restart',
-  refreshonly => true,
-  subscribe   => Package['php5'],
-}
-
-file { '/var/www/html':
-  ensure => directory,
-  owner  => 'www-data',
-  group  => 'www-data',
-  mode   => '0755',
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
